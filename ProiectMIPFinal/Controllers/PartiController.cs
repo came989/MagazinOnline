91ac76a78a582b17679f720a6572d6711ae8e979
LucaciGabriel <114673169+LucaciGabriel@users.noreply.github.com>
2023-01-14 15:17:40 +0200

    Add files via upload

---
+++ ProiectMIPFinal/Controllers/PartiController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProiectMIPFinal.Models;
+
+namespace ProiectMIPFinal.Controllers
+{
+    public class PartiController : Controller
+    {
+        private PartiDbContext partidbctx= new PartiDbContext();
+        // GET: Parti
+        public ActionResult Index()
+        {
+            return View(partidbctx.PartiSet.ToList());
+        }
+
+        [HttpGet]
+
+        public ActionResult Create() 
+        {
+            PartiModel parte= new PartiModel();
+            return View(parte);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(PartiModel parte)
+        {
+            if (ModelState.IsValid)
+            {
+                partidbctx.PartiSet.Add(parte);
+                partidbctx.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View(parte);
+        }
+    }
+}
---
+++ ProiectMIPFinal/Models/PartiModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProiectMIPFinal.Models
+{
+    public class PartiModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+    public class PartiDbContext:DbContext
+    {
+        public DbSet<PartiModel> PartiSet { get; set; }
+    }
+}
