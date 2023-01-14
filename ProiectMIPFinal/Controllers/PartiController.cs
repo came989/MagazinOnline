5ae6106748b49fcaabf684adb6741edfbd1c65b3
LucaciGabriel <114673169+LucaciGabriel@users.noreply.github.com>
2023-01-14 16:15:57 +0200

    Add files via upload

--- ProiectMIPFinal/Controllers/PartiController.cs
+++ ProiectMIPFinal/Controllers/PartiController.cs
@@ -37,5 +37,74 @@ namespace ProiectMIPFinal.Controllers
             }
             return View(parte);
         }
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return HttpNotFound();
+            }
+            PartiModel parte = partidbctx.PartiSet.Find(id);
+            if (null == parte)
+            {
+                return HttpNotFound();
+            }
+            return View(parte);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(PartiModel parte)
+        {
+            if (ModelState.IsValid)
+            {
+                partidbctx.Entry(parte).State = System.Data.Entity.EntityState.Modified;
+                partidbctx.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            return View(parte);
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return RedirectToAction("Index");
+            }
+            PartiModel parte = partidbctx.PartiSet.Find(id);
+            if (null == parte)
+            {
+                return HttpNotFound();
+            }
+            return View(parte);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            if (ModelState.IsValid)
+            {
+
+                PartiModel parte = partidbctx.PartiSet.Find(id);
+                partidbctx.PartiSet.Remove(parte);
+                partidbctx.SaveChanges();
+
+            }
+            return RedirectToAction("Index");
+
+        }
+
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            PartiModel msg = partidbctx.PartiSet.Find(id);
+            if (null == msg)
+            {
+                return HttpNotFound();
+            }
+
+            return View(msg);
+        }
     }
 }
