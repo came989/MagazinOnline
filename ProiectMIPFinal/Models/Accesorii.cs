dbd2bcdb5472c5381d59dd3795831a3bf3b654ca
came989 <45994084+came989@users.noreply.github.com>
2023-01-18 23:56:55 +0200

    Add files via upload

---
+++ ProiectMIPFinal/Models/Accesorii.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProiectMIPFinal.Models
+{
+    public class Accesorii
+    {
+
+        public int ID { get; set; }
+        public int CheieRezerva { get; set; }
+        public string Logo { get; set; }
+        public string ParfumMasina { get; set; }
+    }
+
+    public class AccesoriiDbContext : DbContext
+    {
+        public DbSet<Accesorii> AccesoriiSet { get; set; }
+    }
+}
